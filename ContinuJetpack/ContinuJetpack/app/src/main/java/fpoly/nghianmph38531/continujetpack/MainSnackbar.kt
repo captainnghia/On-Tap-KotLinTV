2231e99535dafb053182ec8d2c10f5625aa2de9f
nghiacteee <minhnghia18042004@gmail.com>
2024-10-17 19:59:19 +0700

    add SnackBar

---
+++ ContinuJetpack/ContinuJetpack/app/src/main/java/fpoly/nghianmph38531/continujetpack/MainSnackbar.kt
@@ -0,0 +1,103 @@
+/*package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    var r by remember { mutableStateOf("") }
+
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState) {
+                data ->
+                Snackbar(
+                    containerColor = Color.Red,
+                    snackbarData = data
+                )
+            }
+        },
+
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                scope.launch {
+                    val result = snackbarHostState.showSnackbar(
+                        "Hello world", "Action",
+                        withDismissAction = true,
+                        SnackbarDuration.Long
+                    )
+                    if(result == SnackbarResult.ActionPerformed){
+                        r = "Action clicked"
+                    } else
+                        r = "Dismissed"
+                }
+            }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "")
+            }
+        }
+    ) {
+        Column(Modifier.padding(it)) {
+            Text(text = r)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 400)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting("Android")
+        }
+    }
+}*/
