797ebfb5ed986d7e21b62cb5169b561e6a838487
nghiacteee <minhnghia18042004@gmail.com>
2025-02-27 00:35:49 +0700

    add location

--- ComposeMaps/ComposeMaps/app/build.gradle.kts
+++ ComposeMaps/ComposeMaps/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -59,4 +60,6 @@ dependencies {
 
     implementation("com.google.maps.android:maps-compose:6.4.4")
     implementation("com.google.android.gms:play-services-maps:19.1.0")
+    implementation ("com.google.accompanist:accompanist-permissions:0.31.1-alpha")
+
 }
